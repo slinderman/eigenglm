20776b1b5f18fc34882dcbdfac06d8db37506d79
Scott Linderman <scott.linderman@gmail.com>
2014-10-17 10:48:43 -0400

    removing the playground version

--- npglm2/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    name = "rectangleapp",
-    # ext_modules=cythonize('*.pyx')
-    ext_modules=cythonize('pynpglm.pyx')
-)
-
